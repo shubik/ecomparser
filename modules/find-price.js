b25a75c9bcc070b5012c37093e36bb8dd0335057
Alexander Farennikov <alex@teradek.com>
2016-01-09 20:14:22 -0800

    Added checks in itemprop="price" and in data-price="NNN"

--- modules/find-price.js
+++ modules/find-price.js
@@ -1,6 +1,32 @@
-var _     = require('lodash'),
-    Utils = require('./utils'),
-    ns    = require('../lib/ns');
+var _          = require('lodash'),
+    Utils      = require('./utils'),
+    ns         = require('../lib/ns'),
+    htmlparser = require('htmlparser2');
+
+
+function getNodeSelector (nodes, suffix) {
+    suffix = suffix || '';
+
+    var selector = _.reduce(nodes.reverse(), function(arr, node) {
+        if (arr._done) return arr;
+
+        if (node.attribs.id) {
+            arr.push('#' + node.attribs.id);
+            arr._done = true;
+        } else {
+            arr.push(node.tagname);
+        }
+
+        return arr;
+    }, []);
+
+    return selector.reverse().join(' ') + suffix;
+}
+
+
+function pricesAreSame (p1, p2) {
+    return Number(p1).toFixed() === Number(p2).toFixed();
+}
 
 
 module.exports = function(data, price) {
@@ -37,13 +63,125 @@ module.exports = function(data, price) {
         retval.type = 'microdata';
         retval.key = namespace;
 
-        return retval;
+        // return retval;
     }
 
-    /* --- 2) does page contain itemprop=\"price\" --- */
+
+    /* --- 2) does page contain itemprop="price" --- */
+
+    /*
+
+     Parse node until we find itemprop="price" node
+
+    */
+
+    var DOMPath = [],
+        targetFound = false,
+        targetClosed = false,
+
+        parser = new htmlparser.Parser({
+            onopentag: function(tagname, attribs){
+                if (!targetFound) {
+
+                    var node = { tagname : tagname, attribs : attribs };
+                    DOMPath.push(node);
+
+                    if (attribs.itemprop && attribs.itemprop === 'price') {
+                        targetFound = true;
+                    }
+                }
+            },
+
+            ontext: function(text){
+                if (targetClosed) return;
+                DOMPath[DOMPath.length - 1].text = text;
+            },
+
+            onclosetag: function(tagname){
+                var lastNode = _.last(DOMPath);
+
+                if (targetFound && !targetClosed) {
+
+                    /* --- Check if content or text equals price --- */
+
+                    if (lastNode.attribs.content && pricesAreSame(lastNode.attribs.content, price)) {
+                        retval.type = 'itemprop';
+                        retval.get = 'nodeContentAttr';
+                        retval.selector = getNodeSelector(DOMPath, '[itemprop="price"]');
+                        targetClosed = true;
+                        return;
+                    } else if (pricesAreSame(Utils.parseNumber(lastNode.text), price)) {
+                        retval.type = 'itemprop';
+                        retval.get = 'nodeText';
+                        retval.selector = getNodeSelector(DOMPath, '[itemprop="price"]');
+                        targetClosed = true;
+                        return;
+                    } else {
+                        targetFound = false;
+                    }
+                }
+
+                if (lastNode.tagname === tagname) {
+                    DOMPath.pop();
+                }
+            }
+        }, { decodeEntities: true });
+
+
+    parser.write(data.html);
+    parser.end();
+
 
 
     /* --- 3) does page contain data-price="NNN" --- */
+
+    var DOMPath = [],
+        targetFound = false,
+        targetClosed = false,
+
+        parser = new htmlparser.Parser({
+            onopentag: function(tagname, attribs){
+                if (!targetFound) {
+
+                    var node = { tagname : tagname, attribs : attribs };
+                    DOMPath.push(node);
+
+                    if (attribs['data-price']) {
+                        targetFound = true;
+                    }
+                }
+            },
+
+            ontext: function(text){
+                if (targetClosed) return;
+                DOMPath[DOMPath.length - 1].text = text;
+            },
+
+            onclosetag: function(tagname){
+                var lastNode = _.last(DOMPath);
+
+                if (targetFound && !targetClosed) {
+
+                    /* --- Check if data-price attribute value equals price --- */
+
+                    if (lastNode.attribs['data-price'] && pricesAreSame(lastNode.attribs['data-price'], price)) {
+                        retval.type = 'data-price';
+                        targetClosed = true;
+                        return;
+                    } else {
+                        targetFound = false;
+                    }
+                }
+
+                if (lastNode.tagname === tagname) {
+                    DOMPath.pop();
+                }
+            }
+        }, { decodeEntities: true });
+
+
+    parser.write(data.html);
+    parser.end();
 
 
     /* --- 4) does page have property=\"v:pricerange\" --- */
@@ -62,4 +200,25 @@ module.exports = function(data, price) {
 
 
     console.log('findPrice() done...');
+
+    return retval;
 }
+
+
+// http://a-techno.com.ua/72767.html -- 1, 2
+// http://www.notus.com.ua/Apple-iMac-215-4K-display-MK452-NEW-2015 -- 2, 5
+// http://portativ.ua/product_98429.html -- 2
+// http://xclusive.com.ua/catalog/macbook/mk452.html -- 3
+// http://maclove.com.ua/catalog/261355/245048/70298 -- 7 class="uah"
+// http://www.sokol.ua/monoblok-apple-imac-a1418-mk452ua-a-065b9/p702209/ -- 2
+// http://foxmart.ua/kompyoutery/apple-imac-a1418-mk452uaa.html -- 2 (text, not content), 4
+// http://pcshop.ua/Monoblok_Apple_iMac_MK452.aspx -- 6
+// http://tid.ua/mikrovolnovaya-pech-gorenje-mo-17-dw-(mo17dw) -- 2 (text)
+// http://myphone.kh.ua/monoblok-apple-imac-215-with-retina-4k-display-mk452-2015/ -- 8, 6
+// http://chooser.com.ua/monoblok-apple-imac-215-with-retina-4k-display-mk452-2015 -- 9
+// http://vivostore.ua/product/apple-new-imac-21-5-retina-mk452-2015 -- 8, 6
+// https://store.iland.ua/apple-computers/personal-computers/imac/imac-21-5-retina-4-k-core-i5-3-1ghz-quad-core-8gb-1tb-intel-iris-pro-6200-mk452.html -- 6
+// http://solvo.com.ua/products/apple-imac-215-with-retina-4k-display-mk452-2015 -- 9
+// http://www.mrfix.com.ua/product/apple-imac-215-with-retina-4k-display-mk452/ -- 6
+// http://mcstore.com.ua/catalog/imac_21_5/apple_imac_21_5_new_mk452/ -- 6
+// http://www.ozon.ru/context/detail/id/18037252/ -- 6
--- test/analyze.js
+++ test/analyze.js
@@ -4,12 +4,16 @@ var ecomparser = require('../index');
 
 
 // var test = ecomparser.analize('http://www.5ok.com.ua/holodilniki/delfa-dbf-150.html');
-var test = ecomparser.analyze('http://www.citrus.ua/shop/goods/tabletpc/262/248181');
+// http://www.citrus.ua/shop/goods/tabletpc/262/248181
+// http://a-techno.com.ua/72767.html
+
+// http://myphone.kh.ua/monoblok-apple-imac-215-with-retina-4k-display-mk452-2015/
+var test = ecomparser.analyze('http://myphone.kh.ua/monoblok-apple-imac-215-with-retina-4k-display-mk452-2015/');
 
 test.done(function(data) {
     // console.log('charset:', data.charset, 'og:', data.opengraph, 'schema:', data.schema, 'URL:', data.url, 'img:', data.image);
 
-    var priceData = ecomparser.findPrice(data, 23799);
+    var priceData = ecomparser.findPrice(data, 43899);
     console.log('priceData:', priceData);
 
 }, function(err) {
